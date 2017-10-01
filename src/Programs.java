6add5bdabccc623099396edef36ea43f09e93347
a.potapov <a.potapov@cardsmobile.ru>
2017-10-01 12:36:39 +0300

    models

---
+++ src/Programs.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+/**
+ * Created by n551jm on 01.10.2017.
+ */
+public class Programs {
+
+    private List<Record> mRecords;
+
+}
---
+++ src/Record.java
@@ -0,0 +1,8 @@
+/**
+ * Created by n551jm on 01.10.2017.
+ */
+public class Record {
+
+
+
+}
